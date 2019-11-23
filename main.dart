9e122b810dce2d73d22d28561a90ccf87f4bf5b1
Sangeeta Jha <43818333+nehasangeetajha@users.noreply.github.com>
2019-11-23 16:22:55 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import './screens/home.dart';
+void main() =>  runApp(new HelloFlutterApp());
+
+class HelloFlutterApp extends StatelessWidget{                                //inherits stateless widget class(generic widget)
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+        title: "Hello Flutter App",
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "Title in App bar",
+            ),
+          ),
+          body: home(),
+        )
+
+    );
+  }
+}
